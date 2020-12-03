077dcf54bcee4676133587bd75b68bc18b2c7556
MZdebel <malgorzata.zdebel@gmail.com>
2020-12-03 19:48:02 +0100

    structure :: initial project structure

---
+++ src/test/java/angua87/recipe/app/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package angua87.recipe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
